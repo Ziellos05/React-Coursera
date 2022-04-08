e3a86c936b5083adc067e84e871bd482f41ef1b1
Ziellos05 <rolandandresortega@gmail.com>
2022-04-08 02:06:24 -0500

    "React-route-dom is ruining my fucking life"

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React, {Component} from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap'
-import Menu from './components/MenuComponent';
-import './App.css';
-import Header from './components/HeaderComponent';
-import Footer from './components/FooterComponent';
-import {DISHES} from './shared/dishes';
+import React, { Component } from "react";
+import Menu from "./components/MenuComponent";
+import "./App.css";
+import Header from "./components/HeaderComponent";
+import Footer from "./components/FooterComponent";
+import { BrowserRouter } from "react-router-dom";
+import { DISHES } from "./shared/dishes";
+import Home from "./components/HomeComponent";
+import { Routes, Route } from "react-router";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dishes: DISHES
+      dishes: DISHES,
     };
   }
 
   render() {
+    const HomePage = () => {
+      return <Home />;
+    };
+
     return (
-        <div className="App">
-          <Header/>
-          <Menu dishes={this.state.dishes}/>
-          <Footer/>
-        </div>
+      <div className="App">
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route exact path="/home" element={<HomePage />} />
+            <Route
+              exact
+              path="/menu"
+              element={<Menu dishes={this.state.dishes} />}
+            />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </div>
     );
   }
 }
