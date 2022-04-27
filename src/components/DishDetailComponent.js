4f281456c613f23a6b57403d50c990c15a37912a
Ziellos05 <rolandandresortega@gmail.com>
2022-04-27 15:37:37 -0500

    Pulling off tha REDUX THUNK D:<

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -3,6 +3,8 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbIte
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { FadeTransform, Fade, Stagger } from 'react-animation-components';
+import { Loading } from './LoadingComponent';
+
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => (val) && (val.length >= len);
@@ -137,7 +139,25 @@ function RenderComments({comments, addComment, dishId}) {
 }
 
 const DishDetail = (props) => {
-  if (props.dish != null) {
+    if (props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else if (props.dish != null)
         return (
             <div className="container">
                 <div className="row">
@@ -159,7 +179,7 @@ const DishDetail = (props) => {
                 </div>
             </div>
         );
-    } else {
+    else {
         return (
             <div></div>
         );
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,7 +8,7 @@ import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import { Routes, Route} from 'react-router-dom'
 import { connect } from 'react-redux';
-import { addComment } from '../redux/ActionCreators';
+import { addComment, fetchDishes } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
   return {
@@ -21,8 +21,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => ({
   
-  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment))
-
+  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
+  fetchDishes: () => { dispatch(fetchDishes())}
 });
 
 class Main extends Component {
@@ -31,17 +31,25 @@ class Main extends Component {
     this.setState({ dishId: dishN });
   }
 
+  componentDidMount() {
+    this.props.fetchDishes();
+  }
+
   render() {
 
     const HomePage = () => {
-      return (<Home dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+      return (<Home dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+      dishesLoading={this.props.dishes.isLoading}
+              dishesErrMess={this.props.dishes.errMess}
       promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
       leader={this.props.leaders.filter((leader) => leader.featured)[0]}/>);
     };
 
     const DishWithId = () => {
       return(
-          <DishDetail dish={this.props.dishes[(window.location.pathname).substr(-1)]}
+          <DishDetail dish={this.props.dishes.dishes[(window.location.pathname).substr(-1)]}
+          isLoading={this.props.dishes.isLoading}
+            errMess={this.props.dishes.errMess}
           comments={this.props.comments.filter((comment) => comment.dishId === parseInt(((window.location.pathname).substr(-1)),10))}
           addComment={this.props.addComment} />
       );
